8f0f6f78f4e5d5862fed54150ae5e3440f76072b
k4m4 <nikolaskam@gmail.com>
2020-09-10 23:56:06 +0300

    Fix inconsistent trailing commas

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,24 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
-        node: true
+        node: true,
     },
     extends: [
         'eslint:recommended',
-        'plugin:react/recommended'
+        'plugin:react/recommended',
     ],
     parserOptions: {
         ecmaFeatures: {
-            jsx: true
+            jsx: true,
         },
         ecmaVersion: 12,
-        sourceType: 'module'
+        sourceType: 'module',
     },
     plugins: [
-        'react'
+        'react',
     ],
     rules: {
         semi: ['error', 'always'],
-        quotes: ['error', 'single']
+        quotes: ['error', 'single'],
     },
 };
