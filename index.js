4a59c34e1d50199ea63057d329ad2d9522e3f428
k4m4 <nikolaskam@gmail.com>
2020-09-10 16:40:41 +0300

    Add Swagger docs to /docs

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
 const { InternalServerError } = require('./utils/errors');
 const apiRouter = require('./routes');
+const openapi = require('./openapi');
 
 const app = express();
 
 app.use(express.json());
 app.use('/api', apiRouter);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(openapi));
 app.use((error, req, res, next) => {
   if (error.status) {
     return res.status(error.status).json(error);
@@ -18,6 +21,6 @@ const PORT = 8080;
 
 app.listen({ port: PORT }, () =>
   console.log(
-    `Server ready at http://localhost:${PORT} with openapi spec visible at http://localhost:${PORT}/api/openapi. Have fun!`
+    `Server ready at http://localhost:${PORT} with docs at http://localhost:${PORT}/docs and OpenAPI spec at http://localhost:${PORT}/api/openapi. Have fun!`
   )
 );
