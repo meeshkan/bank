4923189b204313e914d74ad9ba5016ae15849d1b
k4m4 <nikolaskam@gmail.com>
2020-09-11 12:36:41 +0300

    Add heading to error page

--- components/error.js
+++ components/error.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Stack, Text } from '@chakra-ui/core';
+import { Stack, Heading, Text } from '@chakra-ui/core';
 import Container from './container';
 
 const Error = ({ error }) => {
     return (
         <Container>
             <Stack m="0 auto">
+                <Heading as="h1">An Error has Occured</Heading>
                 <Text>{error.message}</Text>
             </Stack>
         </Container>
